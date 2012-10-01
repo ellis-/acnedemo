79db2b560e5c07fba9d6434a22164eb537fa05cf
Erik Ellis <erik.ellis@gmail.com>
2012-10-01 14:36:01 +0200

    Added index file...

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: erik.ellis
+ * Date: 2012-10-01
+ * Time: 14:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+print "hello acne!!";
